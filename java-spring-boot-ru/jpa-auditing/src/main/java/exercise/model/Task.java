58c3d4fdbe5f1ef17e4798a3729ebcab590314f6
@hexlet/cli <support@hexlet.io>
2025-06-18 09:21:04 +0300

    submit java-spring-boot-ru/jpa-auditing

---
+++ java-spring-boot-ru/jpa-auditing/src/main/java/exercise/model/Task.java
@@ -0,0 +1,43 @@
+package exercise.model;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+// BEGIN
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
+// END
